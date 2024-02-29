368609d996944649a509848f1ddf2cb6e599c23b
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-02-29 14:34:58 +0530

    Bug for wrong properties getting passed to restaurant card component removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,38 @@
 import RestaurantCard from "./RestaurantCard.js";
 import { restaurantList } from "../utils/constants.js";
 
+import { useState } from "react";
 
+/* const [resList, setresList] = useState(restaurantList); */
 
-const Body = () => {
+export const Body = () => {
+
+  const [resList, setresList] = useState(restaurantList);
+
   return (
     <div className="body">
       <div className="search">SEARCH</div>
 
+      <div className="filter-btn">
+
+
+        <button  onClick={() => {
+          const filteredList = restaurantList.filter((restaurant) => restaurant.info.avgRating >= 4.6 );
+          console.log(filteredList)
+          setresList(filteredList) 
+
+        }}>Filter restaurants</button>
+
+
+      </div>
+
       <div className="res-container">
-        {restaurantList.map((restaurant, i) => (
-          
-          <RestaurantCard restaurant={restaurantList[i]} key={restaurant.info.id} />
+        {resList.map((restaurant, i) => (
+          <RestaurantCard
+            restaurant={resList[i]}
+            //initially this loop was running same number of times as there were elements in resList but we were passing wrong props to the Restaurantcard component
+            key={restaurant.info.id}
+          />
         ))}
       </div>
     </div>
@@ -19,3 +40,4 @@ const Body = () => {
 };
 
 export default Body;
+// named and default exports can be used simultaneously for a component
