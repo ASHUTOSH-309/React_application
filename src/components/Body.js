7114d1f2b4c4563c782e039ad36a472bea851312
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-03-25 19:19:38 +0530

    Swiggy api called and shimmer effect included

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,16 +2,40 @@ import RestaurantCard from "./RestaurantCard.js"; // default export=> simple imp
 import { restaurantList } from "../utils/constants.js"; // named export => kavach import
 
 import { useEffect, useState } from "react";
-
+import Shimmer from "./Shimmer.js";
 /* const [resList, setresList] = useState(restaurantList); */
 
 export const Body = () => {
-  const [resList, setresList] = useState(restaurantList);
+  const [resList, setresList] = useState([]);
 
   useEffect(() => {
-
-    console.log("use effect called")
+    //it will be called after component rendering
+    console.log("use effect called");
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8909295&lng=75.7916167&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setresList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (resList.length === 0) {
+    return( 
+    <div>
+    <Shimmer />
+     <Shimmer />
+     <Shimmer />
+    </div>
+    
+    
+    )
+  }
 
   return (
     <div className="body">
