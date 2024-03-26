8f68bf2b2d77679d7017f73476c6ba2d5f4bb3e7
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-03-26 20:16:29 +0530

    Network call,textbox search feature implemented

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,9 @@ import Shimmer from "./Shimmer.js";
 
 export const Body = () => {
   const [resList, setresList] = useState([]);
+  const [textbox_content, settext_box_content] = useState("");
+  const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+  let initial_list = [];
 
   useEffect(() => {
     //it will be called after component rendering
@@ -16,73 +19,53 @@ export const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8909295&lng=75.7916167&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8909295&lng=75.7916167&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
     );
     const json = await data.json();
     console.log(json);
     setresList(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+
+    setfilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    global_data=  json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
   };
 
-  if (resList.length === 0) {
-    return( 
-    <div>
+  // conditional rendering
+  return resList.length === 0 ? (
     <Shimmer />
-     <Shimmer />
-     <Shimmer />
-    </div>
-    
-    
-    )
-  }
-// conditional rendering 
-  return resList.length === 0? (<Shimmer />): (
+  ) : (
     <div className="body">
-      <div className="search">SEARCH</div>
-
-      <div className="filter-btn">
-        <button
-          onClick={() => {
-            const filteredList = restaurantList.filter(
-              (restaurant) => restaurant.info.avgRating >= 4.2
+      <input
+        type="text"
+        value={textbox_content}
+        className="search-textbox"
+        onChange={(event) => {
+          if ((event.target.value.trim() == '')) {
+                console.log("tera dibba khali hai")
+                settext_box_content('')
+                   setfilteredRestaurant(resList);
+          } else {
+            settext_box_content(event.target.value);
+            const newList = resList.filter((restaurant) =>
+              restaurant.info.name
+                .toLowerCase()
+                .includes(textbox_content.toLowerCase())
             );
-            console.log(filteredList);
-            setresList(filteredList);
-          }}
-        >
-          Filter restaurants 4.2
-        </button>
-
-        <button
-          onClick={() => {
-            const filteredList = restaurantList.filter(
-              (restaurant) => restaurant.info.avgRating >= 4.4
-            );
-            console.log(filteredList);
-            setresList(filteredList);
-          }}
-        >
-          Filter restaurants 4.4
-        </button>
-
-        <button
-          onClick={() => {
-            const filteredList = restaurantList.filter(
-              (restaurant) => restaurant.info.avgRating >= 4.6
-            );
-            console.log(filteredList);
-            setresList(filteredList);
-          }}
-        >
-          Filter restaurants 4.6
-        </button>
-      </div>
+            setfilteredRestaurant(newList);
+          }
+        }}
+      />
+      <button className="search" onClick={() => {}}>
+        SEARCH
+      </button>
 
       <div className="res-container">
-        {resList.map((restaurant, i) => (
+        {filteredRestaurant.map((restaurant, i) => (
           <RestaurantCard
-            restaurant={resList[i]}
+            restaurant={filteredRestaurant[i]}
             //initially this loop was running same number of times as there were elements in resList but we were passing wrong props to the Restaurantcard component
             key={restaurant.info.id}
           />
