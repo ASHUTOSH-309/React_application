7a490085e4a8eb8bf7a8b175b3abe821888b0e2d
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-03-26 20:12:59 +0530

    basic routing implemented using createBrowserRouter, Outlet, RouterProvider from react-dom library

--- src/index.js
+++ src/index.js
@@ -3,31 +3,60 @@ import ReactDOM from "react-dom/client";
 
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
+import About from "./components/About.js";
+import { TAB_LOGO } from "./utils/constants.js";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
 
 //large restaurant object taken from swiggy website
-// To access 
+// To access
 //name=> restaurantList[i].info.name
 //cusines=>restaurantList[i].info.cuisines
 //delivery time=> restaurantList[i].info.sla.deliveryTime
-//cost for two=> restaurantList[i].info.costForTwo
 //rating=>restaurantList[i].info.avgRating
+
+//cost for two=> restaurantList[i].info.costForTwo
 //Imageid=>restaurantList[i].info.cloudinaryImageId
 
+//JS CODE TO CHANGE href property of the existing link tab in the index.html
+const tabimage = document.querySelector("#tablogo");
 
-
-
+tabimage.href = TAB_LOGO;
 
 const AppLayout = () => {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,/* associates the component with the home route in this case */
+    errorElement: <Error />,// now the component will be dynamically rendered on the basis of the path which is visited
+    children: [
+
+          {
+            path:"/",
+            element:<Body />
+          },
+          {
+            path: "/about",
+            element: <About />,
+          },
+          {
+            path: "/contact",
+            element: <Contact />,
+          },
+     
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />); 
-
-
+root.render(<RouterProvider router={appRouter} />);
