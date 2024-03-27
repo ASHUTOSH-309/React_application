5eca3f2b04e10fb696a0d1ed4c39c273149f8c05
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-03-27 14:43:46 +0530

    Network call implemented

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const RestaurantMenu = () => {
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8909295&lng=75.7916167&restaurantId=735127&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = data.json();
+        console.log("wo mai hi hu")
+    console.log(json);
+  };
+
   return (
     <div className="menu">
       <h1>Name of the Restaurant</h1>
