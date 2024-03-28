8d5f018bbb42e359e4f0561d1654b7bb9b38e0ea
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-03-28 08:29:02 +0530

    import Use params hook and fetch menu on the basis of restaurant id in the URL

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,48 +1,54 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
- 
- const [resInfo ,setresInfo]=useState(null)
- 
- 
-    useEffect(() => {
+  const [resInfo, setresInfo] = useState(null);
+const {resId}=useParams();
+
+
+
+  useEffect(() => {
     fetchMenu();
   }, []);
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.8909295&lng=75.7916167&restaurantId=735127&catalog_qa=undefined&submitAction=ENTER"
+      MENU_API+resId
     );
     const json = await data.json();
-// await is required even when we are converting the data to json
+    // await is required even when we are converting the data to json
 
     console.log(json);
     setresInfo(json);
-  
   };
 
-if(resInfo===null){
-    return <Shimmer />
+  if (resInfo === null) {
+    return <Shimmer />;
     //without this  the below destructing won't be possible as we might try to destructure from a null resInfo state varible
-}
+  }
 
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.data?.cards[2]?.card?.card?.info;
+  
+    const {cards}=resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR;
+    const { itemCards } =
+    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
 
-
-  const { name,cuisines,costForTwoMessage}=resInfo?.data?.cards[2]?.card?.card?.info; 
- 
-
-
-  return (resInfo === null) ? (<Shimmer />):
-  (
+  return resInfo === null ? (
+    <Shimmer />
+  ) : (
     <div className="menu">
       <h1>{name}</h1>
-      <h3>{cuisines}</h3>
-        <h3>{costForTwoMessage}</h3>
+      <h3>{cuisines.join(", ")}</h3>
+      <h3>{costForTwoMessage}</h3>
       <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
+        {itemCards.map((item) => (
+          <li key={item?.card?.info?.id }>
+            {item?.card?.info?.name} - <span>{item?.card?.info?.price/100 || item?.card?.info?.defaultPrice/100}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
