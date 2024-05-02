c97ed4b93a63e8669de59cd28c5dfb5688809961
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-05-02 20:11:23 +0530

    Different restaurants may require different destructuring

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -4,54 +4,60 @@ import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
-  const [resInfo, setresInfo] = useState(null);
-const {resId}=useParams();
+      const [resInfo, setresInfo] = useState(null);
+      const { resId } = useParams();
 
+      useEffect(() => {
+        fetchMenu();
+      }, []);
 
+      const fetchMenu = async () => {
+        const data = await fetch(MENU_API + resId);
+        const json = await data.json();
+        // await is required even when we are converting the data to json
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
+        console.log(json);
+        setresInfo(json);
+      };
 
-  const fetchMenu = async () => {
-    const data = await fetch(
-      MENU_API+resId
-    );
-    const json = await data.json();
-    // await is required even when we are converting the data to json
+      if (resInfo === null) {
+        return <Shimmer />;
+        //without this  the below destructing won't be possible as we might try to destructure from a null resInfo state varible
+      }
 
-    console.log(json);
-    setresInfo(json);
-  };
+      const { name, cuisines, costForTwoMessage } =
+        resInfo?.data?.cards[2]?.card?.card?.info;
 
-  if (resInfo === null) {
-    return <Shimmer />;
-    //without this  the below destructing won't be possible as we might try to destructure from a null resInfo state varible
-  }
+   //   const { cards } = resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR;
+      const { itemCards } =
+      /* the destructuring pattern for one restaurant need not work for the other */
+        resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[4]?.card
+          ?.card;
+          console.log("haa mai hi h menu")
+          console.log(itemCards)
 
-  const { name, cuisines, costForTwoMessage } =
-    resInfo?.data?.cards[2]?.card?.card?.info;
-  
-    const {cards}=resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR;
-    const { itemCards } =
-    resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card;
+      return resInfo === null ? (
+        <Shimmer />
+      ) : (
+        <div className="menu">
+          <h1>{name}</h1>
+          <h3>{cuisines.join(", ")}</h3>
+          <h3>{costForTwoMessage}</h3>
+          <ul>
+            {itemCards.map((item) => (
 
-  return resInfo === null ? (
-    <Shimmer />
-  ) : (
-    <div className="menu">
-      <h1>{name}</h1>
-      <h3>{cuisines.join(", ")}</h3>
-      <h3>{costForTwoMessage}</h3>
-      <ul>
-        {itemCards.map((item) => (
-          <li key={item?.card?.info?.id }>
-            {item?.card?.info?.name} - <span>{item?.card?.info?.price/100 || item?.card?.info?.defaultPrice/100}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+         
+              <li key={item?.card?.info?.id}>
+                {item?.card?.info?.name} -{" "}
+                <span>
+                  {item?.card?.info?.price / 100 ||
+                    item?.card?.info?.defaultPrice / 100 }
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
 };
 
 export default RestaurantMenu;
